a5ebabe73a07b9eeaee4cb50fc1a83e5f583615c
BedoMaxContab <sicurcalor@gmail.com>
2026-08-21 09:34:32 +0200

    elenco portali piattaforma

---
+++ portali.js
@@ -0,0 +1,2 @@
+// portali della piattaforma - generato 21/08/2026 09:34
+var PORTALI=[{"dati": "https://dati.bedomaxcontab.it/dati.js", "portale": "https://www.sicurcalor.it/consumi"}, {"dati": "https://demo.bedomaxcontab.it/dati.js", "portale": "https://demo.bedomaxcontab.it"}];
